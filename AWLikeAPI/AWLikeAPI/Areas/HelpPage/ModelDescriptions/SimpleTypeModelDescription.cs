eea177a11490e684994785d08e914e36d90eb4b2
Pamellino <34130506+Pamellino@users.noreply.github.com>
2018-02-22 12:20:11 +0100

    Initial commit

---
+++ AWLikeAPI/AWLikeAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AWLikeAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ConnectDB/ToolBox/DataAccess/Database/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox.DataAccess.Database
+{
+    public class Command
+    {
+        internal IDictionary<string, object> Parameters { get; private set; }
+        internal string Query { get; private set; }
+        internal bool IsStoredProcedure { get; private set; }
+
+        public Command(string Query) : this(Query, false)
+        {
+
+        }
+
+        public Command(string Query, bool IsStoredProcedure)
+        {
+            if (string.IsNullOrWhiteSpace(Query))
+                throw new ArgumentException("Query can't be empty or null");
+
+            Parameters = new Dictionary<string, object>();
+            this.Query = Query;
+            this.IsStoredProcedure = IsStoredProcedure;
+        }
+
+        public void AddParameter(string ParameterName, object Value)
+        {
+            Parameters.Add(ParameterName, Value);
+        }
+    }
+}
